66bfd6f5e7b970489d121966f6f773cd27ebf783
Abu Hasan Rumi <30332740+abuhasanrumi@users.noreply.github.com>
2019-11-09 02:28:03 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+
+$(document).ready(function(){
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 0){
+    $('.header').addClass('sticky');
+    }
+    else{
+    $('.header').removeClass('sticky');
+    }
+
+  });
+
+  
+
+  var $grid = $('.portfolio-item').isotope({
+    // options
+  });
+
+  $('.port-nav').on('click', 'li', function(){
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue});
+  });
+
+  $('.port-nav ul li').on('click',function(){
+    $('.port-nav ul li').removeClass('mactive');
+    $(this).addClass('mactive');
+  })
+
+  // Activate Carousel
+  $("#myCarousel").carousel();
+
+  // Enable Carousel Indicators
+  $(".item").click(function(){
+    $("#myCarousel").carousel(1);
+  });
+})
+
+$("#navSameID1,#navSameID2,#navSameID3,#navSameID4,#navSameID5,#navSameID6,#navSameID7").click(function() {
+  $("#myNav").css("width", "0%")
+});
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%"
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%"
+}
+
+
+wow.init()
